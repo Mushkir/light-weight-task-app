5a27af7fa04b5004202084c76608da20953bc67a
Mushkir <mushkirmohamed@gmail.com>
2024-12-26 23:38:49 +0530

    feat: implemented admin panel functionalities using Filament

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         if ($request->user()->role === 'admin') {
-            return redirect('/');
+            return redirect('admin');
         }
 
         return redirect()->intended(route('dashboard', absolute: false));
