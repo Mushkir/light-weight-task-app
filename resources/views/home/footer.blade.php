48ed9ace067a2a8597075d166e9c72c1c55fb695
Mushkir <mushkirmohamed@gmail.com>
2024-12-26 13:54:02 +0530

    style: designed header section

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="p-5 text-center text-sm text-white bg-red-800 mb-5">
+    <p>Copyright&copy; - {{ date('Y') }} | Design and Developed by - <a href="https://github.com/Mushkir"
+            class="hover:underline transition-all" target="_blank">Mohamed
+            Mushkir</a>.
+    </p>
+
+    <span class="mt-2 block"> All rights reserved.</span>
+
+</footer>
